50bd07af0dabd4d9de5c87a8cc8b75a87f46d2b9
hdg0116 <hdg0116@gmail.com>
2026-06-23 02:56:20 +0900

    chore: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import {fileURLToPath} from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +13,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
