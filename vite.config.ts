f16a6801d45ac855b0a7ae766cca1d431ba9e95c
hdg0116 <hdg0116@gmail.com>
2026-06-23 07:31:04 +0900

    chore: 디자인 시스템 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import {fileURLToPath} from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     svgr({
       svgrOptions: {
