3fd3c8fda1a25bd38de70d7de3001e742ce84d53
hdg0116 <hdg0116@gmail.com>
2026-06-25 07:34:47 +0900

    feat: 공통 제목 박스 구현 (#9)

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,3 +2,4 @@ export {default as BottomNavigation} from './BottomNavigation';
 export {default as Button} from './button/Button';
 export {default as Header} from './Header';
 export {default as Input} from './input/Input';
+export {default as PageTitle} from './PageTitle';
