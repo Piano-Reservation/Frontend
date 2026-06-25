baf4fdf968182e0e3e494d068eb206a52853a5fc
sandy <a26948077@gmail.com>
2026-06-25 18:20:22 +0900

    style: 마이페이지 메뉴 포맷팅 적용 (#11)

--- src/pages/mypage/components/MyPageMenuList.tsx
+++ src/pages/mypage/components/MyPageMenuList.tsx
@@ -18,11 +18,7 @@ function MyPageMenuList({menuItems}: MyPageMenuListProps) {
           type='button'
           className='flex h-[53px] w-full items-center justify-between border-b border-[#D8DDEB] px-[15px] text-left last:border-b-0'>
           <span className='text-label1 text-[#222222]'>{item.label}</span>
-          <img
-            src={IcRight}
-            alt='다음 로고'
-            className='h-[20px] w-[20px]'
-          />
+          <img src={IcRight} alt='다음 로고' className='h-[20px] w-[20px]' />
         </button>
       ))}
 
